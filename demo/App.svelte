e830522d19d6606c35ced03310f98d17ed5a1b64
CLDXiang <417198670@qq.com>
2024-03-22 20:40:20 +0800

    try workaround

--- demo/App.svelte
+++ demo/App.svelte
@@ -1,5 +1,13 @@
-<div class="page">
-  <my-component a-boolean={false} />
+<script>
+  let x = false;
+
+  function toggle() {
+    x = !x;
+  }
+</script>
+
+<div class="page" on:click={toggle}>
+  <my-component a-boolean={x} />
 </div>
 
 <style>
@@ -9,10 +17,5 @@
     left: 0;
     right: 0;
     bottom: 0;
-  }
-
-  .page-content {
-    height: 150vh;
-    padding: 0 16px;
   }
 </style>
